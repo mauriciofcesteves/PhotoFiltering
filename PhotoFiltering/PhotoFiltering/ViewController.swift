f4923bdf942064bee96baa9a1eee9f45eebb0e36
Mauricio Esteves <mesteves@kineticcafe.com>
2020-07-17 17:41:01 -0400

    First commit - infra

---
+++ PhotoFiltering/PhotoFiltering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoFiltering
+//
+//  Created by Mauricio Esteves on 2020-07-17.
+//  Copyright © 2020 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
